cd9e0e077d04e025119e13bf09775828c607b369
user1051 <sharma.hitesh31099@gmail.com>
2026-04-09 18:11:40 +0530

    test fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn Jenkins link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Jenkins/i);
+  const linkElement = screen.getByText(/Jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
